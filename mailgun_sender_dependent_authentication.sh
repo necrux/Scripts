8d6ce911d363ee4fd26ea1a435265d3fdb5d6d6f
wes <whendersonii@gmail.com>
2015-01-27 15:42:18 -0600

    Sender Dependent Authentication for Mailgun.

--- mailgun_sender_dependent_authentication.sh
+++ mailgun_sender_dependent_authentication.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 #This is an EXTREMELY dirty bash script for automatically setting up sender dependent authentication for all of your Mailgun domains.
+
+#If Mailgun has already been set up on this server be sure to go into /etc/postfix/main.cf and comment the following lines to avoid duplicate entries:
+#If Mailgun has never been setup or has already been configured properly for this script then no changes need to be made.
+
+#smtp_sasl_auth_enable
+#relayhost
+#smtp_sasl_security_options
+#smtp_sasl_password_maps
+#sender_dependent_relayhost_maps
+#smtp_sender_dependent_authentication
+
+#Below is the format for the 2 files created.
+#/etc/postfix/sasl_passwd
+#    @<domain> postmaster@<domain>:<smtp_password>
+#    @<domain> postmaster@<domain>:<smtp_password>
+#/etc/postfix/sender_relay
+#    @<domain> [smtp.mailgun.org]:587
+#    @<domain> [smtp.mailgun.org]:587
+
 clear
 version=$(grep -o "release [6-7]" /etc/redhat-release|cut -d' ' -f2)
 
