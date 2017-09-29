ab0553281e7118add14c46a026df61745a88738b
wes <whendersonii@gmail.com>
2017-09-28 23:54:26 -0500

    Moved away from one liners; added proper JSON parsing; implemented selection loop; reduced API calls by 50%.

--- servers.sh
+++ servers.sh
@@ -1,48 +1,75 @@
 #!/bin/bash
-#Enumerate all servers on your Rackspace cloud account and establish an SSH connection to one via a selection menu.
+# Enumerate all servers on your Rackspace cloud account and
+# establish an SSH connection to one via a selection menu.
 
-#Fill in with your Rackspace info.
+# Fill in with your Rackspace info.
 USERNAME=
 DDI=
 API=
 
-#Fill in with your SSH info.
+# Fill in with your SSH info.
 SSH_USER=
 SSH_PORT=
-SSH_COMMAND=
 
-clear
-echo "~~~~~~Rackspace Server List Forthcoming~~~~~~"
+Datacenters=(dfw ord syd iad hkg)
+
+# Font color.
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+DEFAULT=$(tput setaf 9)
+NF=$(tput sgr0) #No Formatting.
+
+# Check if a command is installed.
+function is_command {
+  local FAILURE
+
+  for program in $@; do
+    hash ${program} > /dev/null 2>&1
+    if [ $? != 0 ]; then
+      echo "Command not found: ${program}"
+      FAILURE='true'
+    fi
+  done
+
+  if [ ! -z ${FAILURE} ]; then
+    exit 127
+  fi
+}
+
+function server_info {
+  for DC in ${Datacenters[*]}; do
+    curl -s https://${DC}.servers.api.rackspacecloud.com/v2/${DDI}/servers/detail -H "X-Auth-Token: ${TOKEN}" \
+    | jq '.[][] | "\(.name) \(.accessIPv4)"'
+  done
+}
+
+is_command jq
+
+echo -e "${YELLOW}     ~~~~~~ Rackspace Server List Forthcoming ~~~~~~${NF}\n"
 
 TOKEN=$(curl -s \
--X 'POST' \
--d "{\"auth\":{\"RAX-KSKEY:apiKeyCredentials\":{\"username\":\"$USERNAME\", \"apiKey\":\"$API\"}}}" \
--H "Content-Type: application/json" \
-https://identity.api.rackspacecloud.com/v2.0/tokens \
-|python -m json.tool|grep -A5 token|awk -F\" '/id/ {print $4}')
+  -X "POST" \
+  -d "{\"auth\":{\"RAX-KSKEY:apiKeyCredentials\":{\"username\":\"${USERNAME}\", \"apiKey\":\"${API}\"}}}" \
+  -H "Content-Type: application/json" \
+  https://identity.api.rackspacecloud.com/v2.0/tokens \
+  | jq --raw-output .[].token.id)
 
-SERVER_NAMES=($(for DC in dfw ord syd iad hkg; do curl -s https://$DC.servers.api.rackspacecloud.com/v2/$DDI/servers/detail -H "X-Auth-Token: $TOKEN" | python -m json.tool|grep '"name"'|awk -F\" '{print $4}'|sed 's/ /-/g'; done))
+SERVERS=$(server_info)
 
-SERVER_IP=($(for DC in dfw ord syd iad hkg; do curl -s https://$DC.servers.api.rackspacecloud.com/v2/$DDI/servers/detail -H "X-Auth-Token: $TOKEN" | python -m json.tool|grep -A7 '"public"'|egrep -o "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"; done))
-
-count=1
-server=0
-choice=0
-
-echo
-for i in $(echo "${SERVER_NAMES[@]}")
-do 
-    echo -e "$count $i \t ${SERVER_IP[$server]}" && count=$((count + 1)) && server=$((server + 1))
+IFS=$'\n'
+select server in ${SERVERS[@]}; do
+  if [ ! -z "${server}" ]; then
+    selection=${server}
+    break
+  else
+    echo "Incorrect entry."
+  fi
 done
 
-echo -e "\nChoose your server!" 
-read -p ",;-;, >>> " choice
-
-until [ "$choice" -ge 1 -a "$choice" -le "$count" ] 2> /dev/null
-do
-    echo "$choice is not a valid entry. Please try again."
-    echo -e "\nChoose your server!" 
-    read -p ",;-;, >>> " choice
-done
-
-ssh -o StrictHostKeyChecking=no -p$SSH_PORT $SSH_USER@${SERVER_IP[$((choice - 1))]}
+ssh -o StrictHostKeyChecking=no -p${SSH_PORT} ${SSH_USER}@$(echo ${selection} | grep -o "[0-9].*[0-9]")
